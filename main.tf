20e3538d6adfbbb202248d70df9b93456436834b
Keir Badger <keir.badger@acuris.com>
2018-05-03 13:21:40 +0100

    Tmp fix

--- main.tf
+++ main.tf
@@ -13,11 +13,12 @@ resource "aws_iam_role_policy" "role_policy" {
 }
 
 resource "aws_iam_role" "task_role" {
-  name_prefix        = "${
-    length(var.family) <= 32 ?
-      var.family :
-      format("%.24stf%.4s", var.family, sha1(var.family))
-  }"
+//  name_prefix        = "${
+//    length(var.family) <= 32 ?
+//      var.family :
+//      format("%.24stf%.4s", var.family, sha1(var.family))
+//  }"
+  name = "autoscaler"
   assume_role_policy = "${
     var.assume_role_policy == "" ?
       data.aws_iam_policy_document.instance-assume-role-policy.json :
